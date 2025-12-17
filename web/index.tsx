1592b8176aaeed1dc5860eb5f130a7d16f5b814d
deadnah2 <vophuloc1703@gmail.com>
2025-12-17 22:00:04 +0700

    chore: update deps lockfile

--- web/index.tsx
+++ web/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css' // We are using Tailwind, but keeping this import if user adds global css later.
+//import './index.css' // We are using Tailwind, but keeping this import if user adds global css later.
 
 // Since we are using Tailwind via CDN in index.html, we don't strictly need a CSS file for tailwind directives
 // unless we set up PostCSS. For this "runnable" request, CDN is safer.
